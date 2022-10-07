6b00639c2d9ccd7957f570a14eb6c47c1fc4016c
Duy Ta <duyfaker01@gmail.com>
2022-10-07 10:32:56 +0700

    First commit

---
+++ js/script.js
@@ -0,0 +1,10 @@
+document.querySelector('.img-btn').addEventListener('click', function()
+	{
+		document.querySelector('.cont').classList.toggle('s-signup')
+	}
+);
+
+function login(){
+	location.assign("./main.html") ;
+	
+}
